237d531921b4534d9c9e170ab3d72b5153f68d38
tiian58 <tiian58@gmail.com>
2018-10-23 00:37:45 -0300

    many changes hibernate

--- src/main/java/Model/Dj.java
+++ src/main/java/Model/Dj.java
@@ -1,15 +1,34 @@
 package Model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table (name = "DJ")
 public class Dj {
 
-    public String name;
-    public String nickName;
-    public List<Track> tracks;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String nickName;
+    @Transient
+    private List<Track> tracks = new ArrayList<Track>();
+
+    public Dj() {
+    }
 
     public Dj(String name) {
         this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -32,7 +51,15 @@ public class Dj {
         return tracks;
     }
 
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
+
     public void addTrack(Track track){
         tracks.add(track);
+    }
+
+    public String toString(){
+        return "Me llamo " + name + " me conocen como " + nickName;
     }
 }
--- src/test/java/TestDj.java
+++ src/test/java/TestDj.java
@@ -2,22 +2,54 @@ import Model.Dj;
 import Model.Track;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestDj {
 
-    //private Dj martinGarrix;
+    private Dj martinGarrix;
+    private static EntityManager manager;
+    private static EntityManagerFactory emf;
     @Before
     public void loadTestDependencies(){
-        //martinGarrix = new Dj("Martin Garrix");
+        martinGarrix = new Dj("Martin Garrix");
+        emf = Persistence.createEntityManagerFactory("db");
+        manager = emf.createEntityManager();
     }
 
     @Test
     public void addTrack(){
-        //Dj martinGarrix = new Dj("Martin Garrix");
-        //Track animals = new Track("Animals");
-        //martinGarrix.addTrack(animals);
-
-        //assertEquals(1, martinGarrix.tracks.size());
+        martinGarrix.addTrack(new Track("Animals"));
+        assertEquals(1, martinGarrix.getTracks().size());
     }
+
+    @Test
+    public void addDjSQL(){
+        manager.getTransaction().begin();
+
+        manager.persist(martinGarrix);
+        manager.getTransaction().commit();
+
+        manager.close();
+    }
+
+    @Test
+    public void editEntity(){
+        manager.getTransaction().begin();
+
+        Dj martin = manager.find(Dj.class, 7L);
+        martin.setNickName("+X");
+        manager.persist(martin);
+        manager.getTransaction().commit();
+
+        manager.close();
+    }
+
+
 }
